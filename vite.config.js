09dcf62f4ab1d1e6c8b04fccf6e6cd59fde7e956
cafesito <leliasgomer@gmail.com>
2023-02-20 20:05:49 -0500

    upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://cafesit8.github.io/Rick-Morty-React-Vite/",
+  base: "/Rick-Morty-React-Vite/",
   plugins: [react()],
 })
